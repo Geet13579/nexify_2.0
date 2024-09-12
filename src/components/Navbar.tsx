a42b022d0bc83acb073f84c77acd04a7adca9ce4
Geetanjali <69080568+Geet13579@users.noreply.github.com>
2024-09-12 16:29:37 +0530

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,23 +29,26 @@ export default function Navbar() {
   return (
     <>
    
-      <nav className="bg-white flex items-center justify-between font-regular  w-full 2xl:px-60 md:px-10 sm:px-5 h-20 z-40 border-b sticky overflow-hidden top-0">
+      <nav className="bg-white flex items-center justify-between font-regular  w-full 2xl:px-40 md:px-10 sm:px-5 h-20 z-40 border-b sticky overflow-hidden top-0">
         <Link href={'/'}>
           <img alt="logo" className='2xl:w-40 w-24' src={'/logo.svg'}  />
         </Link>
         <div className="flex items-center gap-10">
-          <ul className="hidden lg:flex  items-center justify-center gap-8">
+          <ul className="hidden lg:flex  items-center justify-center gap-8 ">
             {navMenu.map((item) => (
               <Link
                 key={item.label}
                 href={item.link}
-                className={`cursor-pointer hover:text-primary ${
+                className={`cursor-pointer hover:text-primary  ${
                   pathname === item.link
                     ? 'font-medium text-[#686868] 2xl:text-[20px] md:text-[16px]'
                     : 'font-medium text-[#686868] 2xl:text-[20px] md:text-[16px]'
                 }`}
               >
+                <div
+                className="relative text-black hover:text-primary  hover:scale-110 cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-primary before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-primary after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
                 {item.label}
+                </div>
               </Link>
             ))}
           </ul>
